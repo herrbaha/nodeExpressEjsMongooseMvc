30df8cccccf5399fab7ca039ce683be2ce2a8dc0
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-22 23:04:29 +0100

    sass works

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ import path from 'path';
 const app = express();
 const __dirname = path.resolve(path.dirname(''));
 const port = 3044;
+const staticDirectory = path.join(__dirname, './public');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './public/views'));
+app.use(express.static(staticDirectory));
 
 app.get('/', (req, res) => {
 	res.render('index', {
