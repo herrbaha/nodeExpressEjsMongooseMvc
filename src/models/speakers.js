3fb1623eec16db2cdda1ff83f84d292399633290
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-23 01:49:37 +0100

    before two

--- src/models/speakers.js
+++ src/models/speakers.js
@@ -3,8 +3,7 @@ import mongoose from 'mongoose';
 mongoose.connect('mongodb://localhost:27017/mongoConference');
 
 const speakerSchema = mongoose.Schema({
-	name: String,
-	presentation: String
+	name: String
 });
 const SpeakerModel = mongoose.model("Speaker", speakerSchema);
 
